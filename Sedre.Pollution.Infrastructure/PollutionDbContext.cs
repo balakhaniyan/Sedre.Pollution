28147b8acf6654361d4f98336802255320ec4855
B.Dara <B.Dara@DARA>
2021-08-29 10:37:05 +0430

    Init2

---
+++ Sedre.Pollution.Infrastructure/PollutionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sedre.Pollution.Infrastructure
+{
+    public class PollutionDbContext : DbContextBase
+    {
+        
+        public PollutionDbContext(DbContextOptions options) :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
